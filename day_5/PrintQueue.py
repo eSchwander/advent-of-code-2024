661a6a1717e54d54fa401a19017ebd323e3c885e
Evan <evan.schwander@gmail.com>
2024-12-07 00:42:35 -0800

    Day 5

--- day_5/PrintQueue.py
+++ day_5/PrintQueue.py
@@ -27,43 +27,23 @@ class PrintQueue:
         self.good_orders = []
         self.find_good_and_bad_orders()
 
-        self.page_to_node = {}
-        for page_num in self.rules:
-            self.page_to_node[page_num] = self.build_order_tree_node(page_num)
-
-    def build_order_tree_node(self, page):
-        node = TreeNode(page, {})
-
-        # If the page is not in rules, it has no children
-        if page not in self.rules:
-            return node
-
-        # Return the node if we already built it
-        if page in self.page_to_node:
-            return self.page_to_node[page]
-
-        # Build the tree
-        for following_page in self.rules[page]:
-            node.children[following_page] = self.build_order_tree_node(following_page)
-        return node
-
     def find_good_and_bad_orders(self):
         for page_order in self.page_orders:
-            if self.validate_page_order(page_order):
+            if self.is_valid_page_order(page_order):
                 self.good_orders.append(page_order)
             else:
                 self.bad_orders.append(page_order)
 
 
-    def validate_page_order(self, page_order):
+    def is_valid_page_order(self, page_order):
         for i, page in enumerate(page_order):
             if i == len(page_order) - 1:
                 return True
 
-            if not self.validate_page(page, page_order[i+1:]):
+            if not self.is_valid_page(page, page_order[i + 1:]):
                 return False
 
-    def validate_page(self, page, pages_to_check):
+    def is_valid_page(self, page, pages_to_check):
         for page_to_check in pages_to_check:
             if page_to_check in self.reverse_rules[page]:
                 return False
@@ -78,3 +58,27 @@ class PrintQueue:
             middle_page_sum += page_order[middle_index]
 
         return middle_page_sum
+    
+    def evaluate_bad_orders(self):
+        middle_page_sum = 0
+        new_orders = []
+        
+        for bad_order in self.bad_orders:
+            new_order = bad_order.copy()
+            while not self.is_valid_page_order(new_order):
+                i, j = self.find_indexes_to_swap(new_order)
+                if i + j != 0:
+                    new_order[i], new_order[j] = new_order[j], new_order[i]
+            new_orders.append(new_order)
+            middle_index = int(len(new_order) / 2)
+            middle_page_sum += new_order[middle_index]
+
+        return middle_page_sum
+
+    def find_indexes_to_swap(self, page_order):
+        for i, page in enumerate(page_order):
+            for j, page_to_check in enumerate(page_order[:i]):
+                if page_to_check not in self.reverse_rules[page]:
+                    return i, j
+
+        return -1, -1
--- day_5/solution.py
+++ day_5/solution.py
@@ -3,3 +3,4 @@ from PrintQueue import PrintQueue
 pq = PrintQueue('input.txt')
 
 print(pq.evaluate_good_orders())
+print(pq.evaluate_bad_orders())
