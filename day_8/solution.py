e04c23fb7c73405f40fcb1648c5333d8907b6827
Evan <evan.schwander@gmail.com>
2024-12-11 16:38:13 -0800

    d8p2

--- day_8/solution.py
+++ day_8/solution.py
@@ -4,6 +4,7 @@ class Solution:
         self.grid = []
         self.x_len = None
         self.y_len = None
+        self.anti_node_grid = []
 
         with open(filename) as f:
             for line in f:
@@ -12,16 +13,16 @@ class Solution:
                     row.append(c)
                 self.grid.append(row)
 
-    def part_1(self):
+    def get_the_answer(self, part_1):
         self.x_len = len(self.grid[0])
         self.y_len = len(self.grid)
 
-        anti_node_grid = []
+        self.anti_node_grid = []
         for y in range(self.y_len):
             row = []
             for x in range(self.x_len):
                 row.append(False)
-            anti_node_grid.append(row)
+            self.anti_node_grid.append(row)
 
         for y in range(self.y_len):
             for x in range(self.x_len):
@@ -30,20 +31,33 @@ class Solution:
                     matches = self.find_matching_antenna(x, y)
                     for match in matches:
                         match_x, match_y = match
+                        if not part_1:
+                            self.anti_node_grid[match_y][match_x] = True
                         x_dist = x - match_x
                         y_dist = y - match_y
-                        anti_x = x_dist + x
-                        anti_y = y_dist + y
-                        if 0 <= anti_x < self.x_len and 0 <= anti_y < self.y_len:
-                            anti_node_grid[anti_y][anti_x] = True
+                        self.add_antis(x, y, x_dist, y_dist, part_1)
 
         answer = 0
-        for row in anti_node_grid:
+        for row in self.anti_node_grid:
+            formatted_row = ['#' if x else '.' for x in row]
+            print(''.join(formatted_row))
             for x in row:
                 if x:
                     answer += 1
 
+        print('_____________')
+
         return answer
+
+    def add_antis(self, current_x, current_y, x_dist, y_dist, part_1):
+        anti_x = x_dist + current_x
+        anti_y = y_dist + current_y
+
+        if 0 <= anti_x < self.x_len and 0 <= anti_y < self.y_len:
+            self.anti_node_grid[anti_y][anti_x] = True
+            if part_1:
+                return
+            self.add_antis(anti_x, anti_y, x_dist, y_dist, False)
 
     def find_matching_antenna(self, antenna_x, antenna_y):
         antenna = self.grid[antenna_y][antenna_x]
@@ -61,7 +75,8 @@ class Solution:
 
 def main():
     s = Solution('input.txt')
-    print(s.part_1())
+    print(s.get_the_answer(True))
+    print(s.get_the_answer(False))
 
 if __name__ == '__main__':
     main()
--- day_8/test_solution.py
+++ day_8/test_solution.py
@@ -4,6 +4,10 @@ from day_8.solution import Solution
 
 
 class TestSolution(TestCase):
-    def test_solution(self):
+    def test_solution_part_1(self):
         s = Solution('test_input.txt')
-        self.assertEqual(14, s.part_1())
+        self.assertEqual(14, s.get_the_answer(True))
+
+    def test_solution_part_2(self):
+        s = Solution('test_input.txt')
+        self.assertEqual(34, s.get_the_answer(False))
