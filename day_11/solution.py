12364e65a0069c949ed459521aeffab163175a93
Evan <evan.schwander@gmail.com>
2024-12-15 23:25:43 -0800

    Day11 done

--- day_11/solution.py
+++ day_11/solution.py
@@ -5,7 +5,7 @@ memory_map = {}
 
 class Rock:
     def __init__(self, value):
-        self.value = value
+        self.value = int(value)
 
     def live(self):
         string = str(self.value)
@@ -86,12 +86,46 @@ class FileRockRow:
 
         self.row_count.append(rock_count)
 
-if __name__ == "__main__":
+class MapRockRow:
+    def __init__(self, filename):
+        self.row = []
+        self.rock_map = {}
+
+        with open(filename) as f:
+            for x in f.read().strip().split():
+                if x not in self.rock_map:
+                    self.rock_map[x] = 0
+                    self.row.append(Rock(x))
+                self.rock_map[x] += 1
+
+    def step(self):
+        next_rock_map = {}
+
+        for rock in self.row:
+            new_rocks = rock.live()
+            for new_rock in new_rocks:
+                if str(new_rock) not in next_rock_map:
+                    next_rock_map[str(new_rock)] = 0
+                next_rock_map[str(new_rock)] += self.rock_map[str(rock)]
+
+        self.row = []
+        for rock_value in next_rock_map:
+            self.row.append(Rock(rock_value))
+
+        self.rock_map = next_rock_map
+
+    def count_total_rocks(self):
+        count = 0
+        for value in self.rock_map.values():
+            count += value
+        return count
+
+
+def use_rock_row(filename, iterations):
     start_time = time.time()
+    rock_row = RockRow(filename)
 
-    rock_row = RockRow("input.txt")
-
-    for i in range(75):
+    for i in range(iterations):
         step_start_time = time.time()
 
         # print(f"step {i}")
@@ -100,24 +134,48 @@ if __name__ == "__main__":
         rock_row.step()
         step_end_time = time.time()
         step_runtime = step_end_time - step_start_time
-        print(f"step_runtime {i}: {step_runtime} seconds")
+        # print(f"step_runtime {i}: {step_runtime} seconds")
 
     print(f"Runtime: {time.time() - start_time} seconds")
 
     print(len(rock_row.row))
 
-    # file_rock_row = FileRockRow("input.txt")
-    #
-    # for i in range(75):
-    #     step_start_time = time.time()
-    #
-    #     file_rock_row.step(i)
-    #
-    #     step_end_time = time.time()
-    #     step_runtime = step_end_time - step_start_time
-    #     print(f"step_runtime {i}: {step_runtime} seconds")
-    #
-    # end_time = time.time()
-    # runtime = end_time - start_time
-    # print(f"Runtime: {runtime} seconds")
-    # print(file_rock_row.row_count[-1])
+def use_file_rock_row(filename, iterations):
+    start_time = time.time()
+    file_rock_row = FileRockRow(filename)
+
+    for i in range(iterations):
+        step_start_time = time.time()
+
+        file_rock_row.step(i)
+
+        step_end_time = time.time()
+        step_runtime = step_end_time - step_start_time
+        # print(f"step_runtime {i}: {step_runtime} seconds")
+
+    end_time = time.time()
+    runtime = end_time - start_time
+    print(f"Runtime: {runtime} seconds")
+    print(file_rock_row.row_count[-1])
+
+def use_map_rock_row(filename, iterations):
+    start_time = time.time()
+    map_rock_row = MapRockRow(filename)
+
+    for i in range(iterations):
+        step_start_time = time.time()
+
+        map_rock_row.step()
+
+        step_end_time = time.time()
+        step_runtime = step_end_time - step_start_time
+        # print(f"step_runtime {i}: {step_runtime} seconds")
+
+    end_time = time.time()
+    runtime = end_time - start_time
+    print(f"Runtime: {runtime} seconds")
+    print(map_rock_row.count_total_rocks())
+
+if __name__ == "__main__":
+    # use_rock_row('sample_input.txt', 25)
+    use_map_rock_row('input.txt', 75)
